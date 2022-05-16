daa952c8a457e7dca7c3df320e68371dde514ebd
Shrey Jain <shrey.jain711@gmail.com>
2022-05-16 19:10:52 +0530

    treasury page ready

--- src/treasury-0x8c3fa50473065f1d90f186ca8ba1aa76aee409bb.js
+++ src/treasury-0x8c3fa50473065f1d90f186ca8ba1aa76aee409bb.js
@@ -1,9 +1,11 @@
 /* eslint-disable */
 export const dummyTreasuryData = [
-    { name: 'ETH', balance: 3, contractAddress: '', currentPrice: 2800 },
-    {name: 'USDC', balance: 3, contractAddress:"0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", currentPrice: 1},
-    {name: 'USDT', balance: 3, contractAddress:"0xdac17f958d2ee523a2206206994597c13d831ec7", currentPrice: 1},
-    {name: 'DAI', balance: 3, contractAddress:"0x6b175474e89094c44da98b954eedeac495271d0f", currentPrice: 1},
-    {name: 'UNI', balance: 3, contractAddress:"0x1f9840a85d5af5bf1d1762f925bdaddc4201f984", currentPrice: 5},
-    {name: 'MANA', balance: 3, contractAddress:"0x0f5d2fb29fb7d3cfee444a200298f468908cc942", currentPrice: 0.6},
+    { name: 'ETH', balance: 5.6032, contractAddress: '', currentPrice: 2034.43 },
+    { name: 'USDC', balance: 2787334.6620, contractAddress: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', currentPrice: 1.00},
+    { name: 'USDT', balance: 3660.5902, contractAddress: '0xdac17f958d2ee523a2206206994597c13d831ec7', currentPrice: 1.00},
+    { name: 'DAI', balance: 0.9783, contractAddress: '0x6b175474e89094c44da98b954eedeac495271d0f', currentPrice: 1.00},
+    { name: 'UNI', balance: 285.4063, contractAddress: '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984', currentPrice: 5.11},
+    { name: 'MANA', balance: 16.0000, contractAddress: '0x0f5d2fb29fb7d3cfee444a200298f468908cc942', currentPrice: 1.21},
+    { name: 'WBTC', balance: 0.0001, contractAddress: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', currentPrice: 29916.00},
+    { name: 'BAT', balance: 3.0000, contractAddress: '0x0f5d2fb29fb7d3cfee444a200298f468908cc942', currentPrice: 0.40},
 ];
--- src/views/treasury/Treasury.js
+++ src/views/treasury/Treasury.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
+import AddressContext from 'src/context';
 
 import {
   CButton,
@@ -26,52 +27,107 @@ import {
   cilPeople,
 } from '@coreui/icons'
 
-import processedData from 'src/reprocessed-0x8c3fa50473065f1d90f186ca8ba1aa76aee409bb.json'
+import BAT from 'src/assets/images/coins/BAT.png'
+import DAI from 'src/assets/images/coins/DAI.png'
+import ETH from 'src/assets/images/coins/ETH.png'
+import HBAR from 'src/assets/images/coins/HBAR.png'
+import MANA from 'src/assets/images/coins/MANA.png'
+import UNI from 'src/assets/images/coins/UNI.png'
+import USDC from 'src/assets/images/coins/USDC.png'
+import USDT from 'src/assets/images/coins/USDT.png'
+import WBTC from 'src/assets/images/coins/WBTC.png'
+
+import { dummyTreasuryData } from 'src/treasury-0x8c3fa50473065f1d90f186ca8ba1aa76aee409bb.js'
 
 const Treasury = () => {
-    const [addresses, setAddresses] = useState([])
-    const [data, setData] = useState([])
+  const coinImages = {
+    "BAT": BAT,
+    "DAI": DAI,
+    "ETH": ETH,
+    "HBAR": HBAR,
+    "MANA": MANA,
+    "UNI": UNI,
+    "USDC": USDC,
+    "USDT": USDT,
+    "WBTC": WBTC,
+  }
+
+  const {address, setAddress} = useContext(AddressContext);
+  const [addresses, setAddresses] = useState([])
+  const [data, setData] = useState([])
+  const [totalValue, setTotalValue] = useState(0)
   
-    useEffect(() => {
-      setAddresses([
-        ...addresses,
-        '0x8c3fa50473065f1d90f186ca8ba1aa76aee409bb'
-      ])
-    }, [])
-    
-    return (
+  const numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  useEffect(() => {
+    setAddresses([
+      ...addresses,
+      address
+    ])
+    setData(dummyTreasuryData)
+  }, [])
+
+  useEffect(() => {
+    let tv = 0;
+    for (let obj of data){
+      tv += obj.balance * obj.currentPrice
+    }
+    tv = tv.toFixed(2)
+    setTotalValue(tv)
+  }, [data])
+  
+  return (
+    <div style={{display: 'flex', flexDirection: 'column'}}>
+      <div style={{display: 'flex', flexDirection: 'row'}}>
+        <a 
+          href={'https://etherscan.io/address/' + address}
+          target="_blank"
+        >
+          <img className="tx-coin-img" src={coinImages['ETH']} alt={'ETH'} />
+        </a>
+        <p style={{marginLeft: '10px', marginTop: '2px'}}>{address}</p>
+      </div>
+      <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+        <div className='total-value' style={{marginLeft: '5px'}}>All Assets</div>
+        <div>Total Value: <span className='total-value'>{numberWithCommas(totalValue)}</span></div>
+      </div>
       <div>
-        <CTable hover>
-          <CTableHead>
-            <CTableRow>
-              <CTableHeaderCell scope="col">#</CTableHeaderCell>
-              <CTableHeaderCell scope="col">Class</CTableHeaderCell>
-              <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
-              <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
-            </CTableRow>
-          </CTableHead>
-          <CTableBody>
-            <CTableRow>
-              <CTableHeaderCell scope="row">1</CTableHeaderCell>
-              <CTableDataCell>Mark</CTableDataCell>
-              <CTableDataCell>Otto</CTableDataCell>
-              <CTableDataCell>@mdo</CTableDataCell>
-            </CTableRow>
-            <CTableRow>
-              <CTableHeaderCell scope="row">2</CTableHeaderCell>
-              <CTableDataCell>Jacob</CTableDataCell>
-              <CTableDataCell>Thornton</CTableDataCell>
-              <CTableDataCell>@fat</CTableDataCell>
-            </CTableRow>
-            <CTableRow>
-              <CTableHeaderCell scope="row">3</CTableHeaderCell>
-              <CTableDataCell>Larry the Bird</CTableDataCell>
-              <CTableDataCell>Thornton</CTableDataCell>
-              <CTableDataCell>@twitter</CTableDataCell>
-            </CTableRow>
-          </CTableBody>
-        </CTable>
-      </div>);
+        {(data.length > 0) &&
+          <CTable hover>
+            <CTableHead>
+              <CTableRow>
+                <CTableHeaderCell sm="3">Asset</CTableHeaderCell>
+                <CTableHeaderCell sm="3">Price($)</CTableHeaderCell>
+                <CTableHeaderCell sm="3">Amount</CTableHeaderCell>
+                <CTableHeaderCell sm="3">USD Amount</CTableHeaderCell>
+              </CTableRow>
+            </CTableHead>
+            <CTableBody>
+              {data.map((coinObj, index) => {
+                return (
+                  <CTableRow key={index}>
+                    <CTableDataCell>
+                      <a 
+                        href={(coinObj.address !== '')?('https://etherscan.io/token/' + coinObj.contractAddress):('https://etherscan.io/token/' + address)} 
+                        target="_blank"
+                      >
+                        <img className="tx-coin-img" src={coinImages[coinObj.name]} alt={coinObj.name} /> 
+                      </a>{coinObj.name}
+                    </CTableDataCell>
+                    <CTableDataCell>{numberWithCommas(coinObj.currentPrice.toFixed(2))}</CTableDataCell>
+                    <CTableDataCell>{coinObj.balance.toFixed(4)}</CTableDataCell>
+                    <CTableDataCell>{numberWithCommas((coinObj.currentPrice * coinObj.balance).toFixed(2))}</CTableDataCell>
+                  </CTableRow>
+                )
+              })}
+            </CTableBody>
+          </CTable>
+        }
+      </div>
+    </div>
+  );
 }
 
 export default Treasury
