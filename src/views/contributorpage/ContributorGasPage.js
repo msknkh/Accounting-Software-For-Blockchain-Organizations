54aaa0d44f630358dd0753bc38678a67ba4ee164
msknkh <muskankhandelwal369@gmail.com>
2022-05-17 00:17:10 +0530

    Contributor gas fees

--- src/views/contributorpage/ContributorGasPage.js
+++ src/views/contributorpage/ContributorGasPage.js
@@ -9,7 +9,13 @@ import {
   CContainer,
   CRow,
   CCol,
-  CFormInput,  
+  CFormInput, 
+  CTable,
+  CTableHead,
+  CTableRow,
+  CTableHeaderCell,
+  CTableDataCell,
+  CTableBody
 } from '@coreui/react'
 
 import CreatableSelect from 'react-select/creatable';
@@ -24,7 +30,59 @@ import processedData from 'src/reprocessed-0x8c3fa50473065f1d90f186ca8ba1aa76aee
 import TxTable from 'src/components/TxTable'
 
 const ContributorGasPage = () => {
-    return (<div></div>);
+
+  const [signeeGasObj, setSigneeGasObj] = useState({});
+
+  useEffect(() => {
+
+    let signeeGas = {}
+
+    for(let [txHash, txObj] of Object.entries(processedData)){
+        if(txObj.txType === 'Expense'){
+            if(txObj.lastSignee in signeeGas) {
+                signeeGas[txObj.lastSignee].eth += txObj.txCost
+                signeeGas[txObj.lastSignee].usd += txObj.txCostInUSD
+                signeeGas[txObj.lastSignee].signed += 1
+            } else {
+                signeeGas[txObj.lastSignee] = {
+                    eth: txObj.txCost,
+                    usd: txObj.txCostInUSD,
+                    signed: 1,
+                }
+            }
+        }
+    }
+
+    setSigneeGasObj(signeeGas);
+
+  }, [])
+
+  //console.log(signeeGas);
+
+    return (<div>
+      <CTable hover>
+        <CTableHead>
+          <CTableRow>
+            <CTableHeaderCell scope="col">#</CTableHeaderCell>
+            <CTableHeaderCell scope="col">Address</CTableHeaderCell>
+            <CTableHeaderCell scope="col">Total Gas Fees in Eth</CTableHeaderCell>
+            <CTableHeaderCell scope="col">Gas Fees in USD</CTableHeaderCell>
+            <CTableHeaderCell scope="col">Total Transactions Signed</CTableHeaderCell>
+          </CTableRow>
+        </CTableHead>
+        <CTableBody>
+          {Object.entries(signeeGasObj).map(([label, dataObj], index) => {
+            return (<CTableRow>
+            <CTableHeaderCell scope="row">{index}</CTableHeaderCell>
+            <CTableDataCell>{label}</CTableDataCell>
+            <CTableDataCell>{dataObj.eth}</CTableDataCell>
+            <CTableDataCell>{dataObj.usd}</CTableDataCell>
+            <CTableDataCell>{dataObj.signed}</CTableDataCell>
+          </CTableRow>)
+          })}
+        </CTableBody>
+      </CTable>
+    </div>);
 }
 
 export default ContributorGasPage
