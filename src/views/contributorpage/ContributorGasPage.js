117d1d8bdfd9c4ff968c58867bde194a0a100def
Shrey Jain <shrey.jain711@gmail.com>
2022-05-17 01:48:21 +0530

    contributor page beatified

--- src/views/contributorpage/ContributorGasPage.js
+++ src/views/contributorpage/ContributorGasPage.js
@@ -29,6 +29,9 @@ import {
 import processedData from 'src/reprocessed-0x8c3fa50473065f1d90f186ca8ba1aa76aee409bb.json'
 import TxTable from 'src/components/TxTable'
 
+import ETH from 'src/assets/images/coins/ETH.png'
+
+
 const ContributorGasPage = () => {
 
   const [signeeGasObj, setSigneeGasObj] = useState({});
@@ -60,6 +63,21 @@ const ContributorGasPage = () => {
   //console.log(signeeGas);
 
     return (<div>
+      <div style={{marginBottom:'20px'}}>
+        <CContainer>
+          <CRow>
+            <CCol sm="auto">
+              <a 
+                href={'https://etherscan.io/address/0x8c3fa50473065f1d90f186ca8ba1aa76aee409bb'}
+                target="_blank"
+              >
+                <img className="tx-coin-img" src={ETH} alt={'ETH'} />
+              </a>
+            </CCol>
+            <CCol sm="auto">0x8c3fa50473065f1d90f186ca8ba1aa76aee409bb</CCol>
+          </CRow>
+        </CContainer>
+      </div>
       <CTable hover>
         <CTableHead>
           <CTableRow>
@@ -74,9 +92,9 @@ const ContributorGasPage = () => {
           {Object.entries(signeeGasObj).map(([label, dataObj], index) => {
             return (<CTableRow>
             <CTableHeaderCell scope="row">{index}</CTableHeaderCell>
-            <CTableDataCell>{label}</CTableDataCell>
-            <CTableDataCell>{dataObj.eth}</CTableDataCell>
-            <CTableDataCell>{dataObj.usd}</CTableDataCell>
+            <CTableDataCell><a target="_blank" href={'https://etherscan.io/address/'+label}>{label}</a></CTableDataCell>
+            <CTableDataCell>{dataObj.eth.toFixed(4)}</CTableDataCell>
+            <CTableDataCell>{dataObj.usd.toFixed(2)}</CTableDataCell>
             <CTableDataCell>{dataObj.signed}</CTableDataCell>
           </CTableRow>)
           })}
