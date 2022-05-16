e5b9d3275eb721ef58946e2d70fca2609d806576
Shrey Jain <shrey.jain711@gmail.com>
2022-05-16 14:23:35 +0530

    fixed bugs & finished csv maker

---
+++ src/treasury-0x8c3fa50473065f1d90f186ca8ba1aa76aee409bb.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export const dummyTreasuryData = [
+    { name: 'ETH', balance: 3, contractAddress: '', currentPrice: 2800 },
+    {name: 'USDC', balance: 3, contractAddress:"0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", currentPrice: 1},
+    {name: 'USDT', balance: 3, contractAddress:"0xdac17f958d2ee523a2206206994597c13d831ec7", currentPrice: 1},
+    {name: 'DAI', balance: 3, contractAddress:"0x6b175474e89094c44da98b954eedeac495271d0f", currentPrice: 1},
+    {name: 'UNI', balance: 3, contractAddress:"0x1f9840a85d5af5bf1d1762f925bdaddc4201f984", currentPrice: 5},
+    {name: 'MANA', balance: 3, contractAddress:"0x0f5d2fb29fb7d3cfee444a200298f468908cc942", currentPrice: 0.6},
+];
--- src/views/treasury/Treasury.js
+++ src/views/treasury/Treasury.js
@@ -21,10 +21,38 @@ import {
 } from '@coreui/icons'
 
 import processedData from 'src/reprocessed-0x8c3fa50473065f1d90f186ca8ba1aa76aee409bb.json'
-import TxTable from 'src/components/TxTable'
 
-const Treasury = () => {
-    return (<div></div>);
+import { useMoralisWeb3Api } from "react-moralis";
+import { AddressBookQuery } from '@hashgraph/sdk';
+
+
+const Treasury = (props) => {
+  const Web3Api = useMoralisWeb3Api();
+
+  const [addresses, setAddresses] = useState([])
+  const [data, setData] = useState([])
+
+  const getTokenBalances = async (address) => {
+    const options = {
+      chain: 'eth',
+      address: address
+    }
+    const balances = await Web3Api.getAllERC20(options);
+    setData((prevData) => [...prevData, balances]);
+  }
+
+  useEffect(() => {
+    setAddresses([
+      ...addresses,
+      '0x8c3fa50473065f1d90f186ca8ba1aa76aee409bb'
+    ])
+    for (let address of addresses){
+      getTokenBalances(address);
+    }
+    console.log(data);
+  }, [])
+
+  return (<div></div>);
 }
 
 export default Treasury
